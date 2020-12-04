d536efda9b95ed8dfab37a805e7c47825852abe0
Gethi <bigard.antoine@gmail.com>
2020-12-04 16:52:19 +0100

    commit fuck u

--- web/index.ts
+++ web/index.ts
@@ -145,13 +145,14 @@ const imageLightOptions: DropdownOption[] = [
     { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg' },
     { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-black-logo.svg' },
     { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-color-logo.svg' },
+    { text: 'Sbs', value: 'https://dev.sidebyside.live/sidebyside-logo-long-black.svg' },
 ];
 
 const imageDarkOptions: DropdownOption[] = [
-
     { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg' },
     { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-white-logo.svg' },
     { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-bw-logo.svg' },
+    { text: 'Sbs', value: 'https://dev.sidebyside.live/sidebyside-logo-long-white.svg' },
 ];
 
 const widthOptions = [
